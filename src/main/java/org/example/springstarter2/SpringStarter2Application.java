b1f26a42c0f8b014df6a69ae473d191cee3fa735
SuhaibRashad <mdsuhaibengineer@gmail.com>
2026-06-17 14:41:11 +0530

    Initial commit

---
+++ src/main/java/org/example/springstarter2/SpringStarter2Application.java
@@ -0,0 +1,13 @@
+package org.example.springstarter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarter2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStarter2Application.class, args);
+    }
+
+}
