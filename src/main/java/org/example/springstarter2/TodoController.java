b81dca2215f2a3d31e3f532555c56dcf836cdec5
SuhaibRashad <mdsuhaibengineer@gmail.com>
2026-06-17 15:41:44 +0530

    Adding Response Status

--- src/main/java/org/example/springstarter2/TodoController.java
+++ src/main/java/org/example/springstarter2/TodoController.java
@@ -1,9 +1,7 @@
 package org.example.springstarter2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +24,7 @@ public class TodoController {
     }
 
     @PostMapping("/todos")
+    @ResponseStatus(HttpStatus.CREATED)
     public List<Todo> createTodo(@RequestBody Todo newTodo){
         todoList.add(newTodo);
         return todoList;
